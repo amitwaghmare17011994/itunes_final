fbc1791d7a96eff13b3b0c781f1ddea03861d729
amitwaghmaretorinit <amit@torinit.ca>
2021-04-19 01:15:34 +0530

    lazy load song added

--- src/Apis/songsApi.js
+++ src/Apis/songsApi.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export const getSongs = async (term = '',offset=0) => {
     try {
-        const res = await axios.get(`https://itunes.apple.com/search/?term=${term}&offset=${offset}&limit=50`)
+        const res = await axios.get(`https://itunes.apple.com/search/?term=${term}&offset=${offset}&limit=10`)
         return res.data.results
     } catch (err) {
         return []
